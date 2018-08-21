96a8b823fe462fddcf124f2435d24cfb3f290508
Nikhil Bhole <nrbhole21@gmail.com>
2018-08-21 12:48:12 +0530

    Timer implemented in angular. radio buttons data binding done

--- src/main/resources/static/Javascript/home.js
+++ src/main/resources/static/Javascript/home.js
@@ -1,35 +1,72 @@
 var app = angular.module("jsbin", ['ngMaterial']);
 
-app.controller("DemoCtrl", function($scope,$http) {
-	  
-	  var vm = this;
-	  var URL = 'getQuestions';
-	  var request = {
-		  method: 'GET',
-		  url: URL,
-	  };
-	  
-	  
-	  $http(request).then(function(response) {
-	    	$scope.vm.data = response.data;
-	    	$scope.index = 0;
+app.filter('secondsToDateTime', function() {
+    return function(seconds) {
+        return new Date(1970, 0, 1).setSeconds(seconds);
+    };
+})
+
+app.controller("DemoCtrl", function($scope,$http,$interval) {
+	var intervalId;
+	$scope.Answers = {};
+	$scope.counter = 0;
+	$scope.initialCountdown = 10;
+	$scope.countdown = $scope.initialCountdown;
+	
+	$scope.getData = function(){
+		$http({
+			url: 'getQuestions',
+			method: 'GET'
+		}).then(function(resp){
+			
+			$scope.questions = resp.data;
+			$scope.index = 0;
 	    	
-	    	$scope.complaints = vm.data[0];
+	    	$scope.complaints = $scope.questions[0];
 	    	
 	    	$scope.getNext = function (){
-	  	    $scope.index = $scope.index + 1;
-	  	    $scope.complaints = $scope.vm.data[$scope.index];
+		  	    $scope.index = $scope.index + 1;
+		  	    $scope.complaints = $scope.questions[$scope.index];
 	  	    }
 
 	  	  	$scope.getPrevious = function (){
-	  	    $scope.index = $scope.index - 1;
-	  	    $scope.complaints = $scope.vm.data[$scope.index];
+		  	    $scope.index = $scope.index - 1;
+		  	    $scope.complaints = $scope.questions[$scope.index];
 	  	    }
-	   }).
-	  catch(function(response) {
-	    	vm.data = response.data;
-	  });
-	  
+		}, function(resp){
+			alert("Error Occoured.");
+		});
+	};
+	
+	$scope.getUserResponse = function(){
+		console.log($scope.Answers)
+	};
+		
+	$scope.timer = function(){
+	    var startTime = new Date();
+	    intervalId = $interval(function(){
+	        var actualTime = new Date();
+	        $scope.counter = Math.floor((actualTime - startTime) / 1000);
+	        $scope.countdown = $scope.initialCountdown - $scope.counter;
+	    }, 1000);
+	 };
 	 
-	  
+	 $scope.$watch('countdown', function(countdown){
+		    if (countdown === 0){
+		        $scope.stop();
+		    }
+	 });
+
+	$scope.start = function(){
+		    $scope.timer();
+	};
+
+	$scope.stop = function(){
+		    $interval.cancel(intervalId);
+	};
+	
+	
+	
+	
+	
 });
